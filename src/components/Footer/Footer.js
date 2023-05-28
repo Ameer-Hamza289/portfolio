bec7c05ba7a87b6e0c2ad7d4453bafceb4ffe233
Ameer Hamza <ameer.hamza102002@gmail.com>
2023-05-28 23:41:28 +0500

    Final Commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rishunayak/rishunayak.github.io'
+      href='https://github.com/Ameer-Hamza289/portfolio'
       className='link footer__link' target="_blank"
     >
     ©2023 All rights reserved - Ameer Hamza
