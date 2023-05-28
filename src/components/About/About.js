274010ca6f1b39e279fc11a909c6107ca5e114cd
Ameer Hamza <ameer.hamza102002@gmail.com>
2023-05-28 21:06:57 +0500

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,159 @@
+
+import SchoolIcon from "@material-ui/icons/School";
+import StarRateIcon from "@material-ui/icons/StarRate";
+  import { ThemeContext } from '../../contexts/theme';
+  import { useContext } from 'react';
+  import { useState } from 'react';
+  import { useEffect } from 'react';
+import {BiStar} from "react-icons/bi"
+
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+
+import './About.css'
+
+
+
+function About  () {
+  
+
+  const [{ themeName }] = useContext(ThemeContext);
+  const [linecolor, setlinecolor] = useState(
+    themeName === "light" ? "#23283e" : "#fcfcfc"
+  );
+
+ useEffect(() => {
+    if (themeName === "dark") {
+      setlinecolor("#fcfcfc");
+    } else {
+      setlinecolor("#23283e");
+    }
+  }, [themeName]);
+ 
+
+  return (
+     
+    <div className='about center' id='about'>
+      
+      <h2>ABOUT <span className='about__name'> ME</span></h2>     
+   
+    
+    
+      <div className='about__contact' >
+
+                 
+                 <div>
+        
+                   <img src='' alt='Ameer Hamza' id='profile'/>
+                   {/* <img src='https://avatars.githubusercontent.com/u/57229844?v=4' alt='Ameer Hamza' id='profile'/> */}
+    
+                 </div>
+
+                <div>
+                    <h4 className='about__desc'>
+                      Hi Everyone, I am Ameer Hamza from Lahore, Pakistan.
+                      I am currently pursuing in Bachelor of Information Technology from University of Education Township, Lahore.
+                    </h4>
+                    <br/>
+                  <div id='interest' >
+                   <h4 className='about__desc'> Some of my interests apart form Coding :</h4>
+               
+                   <BiStar className='about__name'/> <span className='about__name'> Listing Music</span>
+                   <br/>
+                   <BiStar className='about__name'/> <span className='about__name'> Playing Badminton </span>
+                   <br/>
+                   <BiStar className='about__name'/> <span className='about__name'> Watching Cricket</span>
+                   <br/>
+                    <BiStar className='about__name' /> <span className='about__name'>Watching Movies</span>
+                    </div>
+                </div>
+                 
+          </div>     
+                  
+
+           
+            
+               
+                
+      <h2 id='timeline'>MY <span className='about__name'>EDUCATION</span> </h2>
+
+     
+      <VerticalTimeline lineColor={linecolor}>
+        <VerticalTimelineElement date='2021 - 2025'  icon={<SchoolIcon />} contentStyle={{
+              boxShadow: `var(--shadow)`,
+              backgroundColor: `var(--clr-bg)`,
+              textAlign: "center",
+              color: `var(--clr-fg-alt)`
+            }}
+            contentArrowStyle={{ borderRight: "16px solid  rgb(33, 150, 243)" }}
+            iconStyle={{
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`
+            }}
+            >
+      
+            <h4
+              className="vertical-timeline-element-subtitle"
+              data-aos="fade-right"
+            >
+              University Of Education - Township
+            </h4>
+            <p data-aos="fade-right">
+              Joined the University to embrace the opportunities ahead and make
+               the most of my time to nurture my skills and knowledge in the IT field.
+                Also learned many soft skills in the process, still in progress.
+            </p>
+
+        </VerticalTimelineElement>
+
+
+        <VerticalTimelineElement date='June 2018 - June 2021'  icon={<SchoolIcon />} contentStyle={{
+              boxShadow: `var(--shadow)`,
+              backgroundColor: `var(--clr-bg)`,
+              textAlign: "center",
+              color: `var(--clr-fg-alt)`
+            }}
+            contentArrowStyle={{ borderRight: "16px solid  rgb(33, 150, 243)" }}
+            iconStyle={{
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`
+            }}
+            >
+    
+            <h4
+              className="vertical-timeline-element-subtitle"
+              data-aos="fade-right"
+            >
+              Bachelor of Computer Application (BCA)
+            </h4>
+            <p data-aos="fade-right">
+               Babu Banarasi Das University, Lucknow
+            </p>
+
+        </VerticalTimelineElement>
+
+
+        <VerticalTimelineElement
+            icon={<StarRateIcon />}
+            iconStyle={{
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`
+            }}
+          ></VerticalTimelineElement>
+
+      </VerticalTimeline>
+              
+           
+
+    
+
+    
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rishunayak/rishunayak.github.io'
+      className='link footer__link' target="_blank"
+    >
+    ©2023 All rights reserved - Ameer Hamza
+    </a>
+  </footer>
+)
+
+export default Footer
